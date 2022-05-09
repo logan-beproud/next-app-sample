0c197ec8835aa8b86d890a8aecb7cfe867437739
logan-beproud <logan.beproud@krosslab.io>
2022-05-09 10:14:01 +0900

    deployment test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Logan!! Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Fanto Gazua!! </a>
         </h1>
 
         <p className={styles.description}>
